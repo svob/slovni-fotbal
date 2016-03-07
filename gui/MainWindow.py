30f6eaa701afbe546eeba4c1f5317b189dc50b3d
Philae <philae.free@gmail.com>
2016-03-07 13:24:29 +0100

    Initial commit

---
+++ gui/MainWindow.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'mainWindow.ui'
+#
+# Created by: PyQt4 UI code generator 4.11.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.setWindowModality(QtCore.Qt.NonModal)
+        MainWindow.resize(473, 695)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
+        MainWindow.setSizePolicy(sizePolicy)
+        MainWindow.setMinimumSize(QtCore.QSize(473, 695))
+        MainWindow.setMaximumSize(QtCore.QSize(473, 695))
+        MainWindow.setUnifiedTitleAndToolBarOnMac(False)
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.lblImage = QtGui.QLabel(self.centralwidget)
+        self.lblImage.setGeometry(QtCore.QRect(35, 42, 401, 111))
+        self.lblImage.setText(_fromUtf8(""))
+        self.lblImage.setObjectName(_fromUtf8("lblImage"))
+        self.btnLoad = QtGui.QPushButton(self.centralwidget)
+        self.btnLoad.setGeometry(QtCore.QRect(30, 180, 411, 23))
+        self.btnLoad.setObjectName(_fromUtf8("btnLoad"))
+        self.btnDelete = QtGui.QPushButton(self.centralwidget)
+        self.btnDelete.setGeometry(QtCore.QRect(30, 650, 411, 23))
+        self.btnDelete.setObjectName(_fromUtf8("btnDelete"))
+        self.listWidget = QtGui.QListWidget(self.centralwidget)
+        self.listWidget.setGeometry(QtCore.QRect(30, 220, 411, 411))
+        self.listWidget.setObjectName(_fromUtf8("listWidget"))
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(_translate("MainWindow", "Slovní fotbal", None))
+        self.btnLoad.setText(_translate("MainWindow", "Načíst písmena", None))
+        self.btnDelete.setText(_translate("MainWindow", "Odstranit slovo", None))
+
